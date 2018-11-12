8d2fca0aa56be9b409991a51f92960ec96610ffa
brennanclark <brennanpclark@gmail.com>
2018-11-12 20:43:52 +0000

    Adds a test for the product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "validates the prsence of a name" do
+
+    end
+    it "validates the prsence of a price" do
+
+    end
+    it "validates the prsence of a quantity" do
+
+    end
+    it "validates the prsence of a category" do
+
+    end
+  end
+end
